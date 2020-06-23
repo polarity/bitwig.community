ea634f34216b31d6fe3b95483335c83d81767de7
Robert Agthe <robert@scriptshit.de>
2020-06-23 06:51:26 +0200

    linking bitwig image

--- src/components/presets/index.js
+++ src/components/presets/index.js
@@ -20,7 +20,7 @@ export default ({ children, limit, presets }) => {
         <Typography>
           <h3>{startCase(file.name)}</h3>
           <p>by {file.user.username}</p>
-          <img style={{ maxWidth: '100%', marginBottom: '10px' }} src='made-with-bitwig.png' />
+          <img style={{ maxWidth: '100%', marginBottom: '10px' }} src='/made-with-bitwig.png' alt='Made with Bitwig Logo' />
           <img style={{ maxWidth: '60px', float: 'left', marginRight: '10px' }} src={file.user.avatarURL} alt='avatar' />
           <p>{file.desc} Click below to download this Bitwig preset! by {file.user.username} <span>💾</span>&nbsp;<a href={'https://github.com/polarity/bitwig-community-presets/raw/master/' + file.name} title={'Download ' + file.name + ' Preset'}>Download</a></p>
         </Typography>
