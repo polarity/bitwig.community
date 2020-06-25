7fc82e7b42576e7e1fbee645c78ac92e7e64b205
Robert Agthe <robert@scriptshit.de>
2020-06-25 05:46:37 +0200

    yt link support for preset page

--- src/components/presets/index.js
+++ src/components/presets/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { map, startCase } from 'lodash'
 import Typography from '../typography'
 import styles from './styles.module.css'
+import Youtube from '../youtube'
 
 const Limit = (data, limit) => {
   if (limit) {
@@ -18,11 +19,13 @@ export default ({ children, limit, presets }) => {
     return (
       <article style={{ border: '1px solid #666', backgroundColor: '#333' }}>
         <Typography>
-          <h3>{startCase(file.name.split('.')[0])}</h3>
+          <h3 style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>{startCase(file.name.split('.')[0])}</h3>
           <p>by {file.user.username}</p>
-          <img style={{ maxWidth: '100%', marginBottom: '10px' }} src='/made-with-bitwig.png' alt='Made with Bitwig Logo' />
+          {!file.videoYoutube && <img style={{ maxWidth: '100%', marginBottom: '10px' }} src='/made-with-bitwig.png' alt='Made with Bitwig Logo' />}
+          {file.videoYoutube && <Youtube key={file.id} title={file.name.split('.')[0]} link={file.videoYoutube} />}
           <img style={{ maxWidth: '60px', float: 'left', marginRight: '10px' }} src={file.user.firebaseUrl} alt='avatar' />
-          <p>{file.desc} Click below to download this Bitwig preset! by {file.user.username} <span>💾</span>&nbsp;<a href={file.download} title={'Download ' + file.name + ' Preset'}>Download</a></p>
+          <p>{file.desc} Click below to download this Bitwig preset! by <b>{file.user.username}</b></p>
+          <p><span>💾</span>&nbsp;<a href={file.download} title={'Download ' + file.name + ' Preset'}>Download</a></p>
         </Typography>
       </article>
     )
