38fe2f1cd9fd60f1370a01525b358fadd2659fd9
Robert Agthe <robert@scriptshit.de>
2020-07-06 04:18:43 +0200

    og image problem fixed

--- src/templates/preset-creator.js
+++ src/templates/preset-creator.js
@@ -7,7 +7,7 @@ import SignIn from '../components/sign-in'
 import { mapper } from '@reduxless/react'
 import Presets from '../components/presets'
 import getYoutubeImage from '../utils/getYoutubeImage'
-
+import urlSlug from 'url-slug'
 const Mapped = mapper({
   modalLoginState: store => store.get('modalLoginState')
 }, {
@@ -22,13 +22,12 @@ export default Mapped(({ modalLoginState, pageContext }) => {
         <meta charSet='utf-8' />
         <title>{presets[0].user.username + '´s Presets for Bitwig Studio'}</title>
         <meta name='viewport' content='width=device-width' />
-        <meta property='og:url' content='https://bitwig.community' />
+        <meta property='og:url' content={'https://bitwig.community/creator-' + urlSlug(fp.user.username)} />
         <meta property='og:title' content={presets[0].user.username + '´s Presets for Bitwig Studio'} />
         <meta property='og:description' content={'Browse and download many many presets for Bitwig Studio created by the ' + presets[0].user.username} />
         <meta property='og:image:width' content='1080' />
         <meta property='og:image:height' content='1080' />
         <meta property='og:type' content='website' />
-        <meta property='og:image' content='https://bitwig.community/bitwig-for-life.jpg' />
         {!fp.videoYoutube && <meta property='og:image' content='https://bitwig.community/bitwig-for-life.jpg' />}
         {fp.videoYoutube && <meta property='og:image' content={getYoutubeImage(fp.videoYoutube, 'max')} />}
       </Helmet>
@@ -36,7 +35,7 @@ export default Mapped(({ modalLoginState, pageContext }) => {
       <Header />
       <SignIn />
 
-      <SectionHeader h={presets[0].user.username}>
+      <SectionHeader h={fp.user.username}>
       Download Presets for Bitwig Studio by {pageContext.presets[0].user.username}.
         <br />Want to add your Preset? Join our <a href='https://discord.gg/nGgWY7w'>Bitwig discord</a> and drag &amp; drop your files.
       </SectionHeader>
