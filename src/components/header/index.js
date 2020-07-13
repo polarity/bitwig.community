a3ffa007c6ca67c77a466babdebd67e936f01ac0
Robert Agthe <robert@scriptshit.de>
2020-07-13 19:48:13 +0200

    better general header menu with dashboard link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,6 +4,7 @@ import Login from '../login'
 import { mapper } from '@reduxless/react'
 import Typography from '../typography'
 import Logout from '../logout'
+import { Link } from 'gatsby'
 
 const Mapped = mapper({
   loggedInUser: store => store.get('loggedInUser')
@@ -15,13 +16,15 @@ export default Mapped(({ children, loggedInUser }) =>
     <a href='https://bitwig.community'><img alt='Bitwig Community Logo by Amadeus Paulussen' src='/bitwig-logo.png' className={styles.img} /></a>
     <Typography>
       {!loggedInUser && <Login />}
-      {loggedInUser && <Logout><span>welcome {loggedInUser.displayName}</span></Logout>}
+      {loggedInUser && <Logout><span>Welcome <b>{loggedInUser.displayName}</b></span></Logout>}
     </Typography>
     <Typography>
-      <a href='/' className={styles.NavLinks}>Startpage</a>
-      &nbsp;| <a href='/gridniks/' className={styles.NavLinks}>Gridniks</a>
-      &nbsp;| <a href='/presets' className={styles.NavLinks}>Presets</a>
-      &nbsp;| <a href='/wiki/' className={styles.NavLinks}>BitWiki</a>
+      {loggedInUser && <Link to='/dashboard' className={styles.NavLinks}><b>Dashboard</b></Link>}
+      {loggedInUser && <span>&nbsp;|| </span>}
+      <Link to='/' className={styles.NavLinks}>Startpage</Link>
+      &nbsp;| <Link to='/gridniks/' className={styles.NavLinks}>Gridniks</Link>
+      &nbsp;| <Link to='/presets' className={styles.NavLinks}>Presets</Link>
+      &nbsp;| <Link to='/wiki/' className={styles.NavLinks}>BitWiki</Link>
     </Typography>
   </header>
 )
