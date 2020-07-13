fb58178ddeb563b8aca1f7abfe493d6312c211e1
Robert Agthe <robert@scriptshit.de>
2020-07-13 16:55:25 +0200

    hide input after save

--- src/components/text-input-toggle/index.js
+++ src/components/text-input-toggle/index.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react'
 import firebase from 'firebase/app'
 
-const storeDoc = async (value, doc, keyString) => {
+const storeDoc = async (value, doc, keyString, setToggle) => {
   doc[keyString] = value
   try {
-    const docRef = await firebase
+    await firebase
       .firestore()
       .collection('presets')
       .doc(doc.id)
       .set(doc)
-    console.log('doc saved ', docRef)
+    setToggle(false)
+    console.log('doc saved ')
   } catch (error) {
     console.log('something went wrong ', error)
   }
 }
 
-export default ({ keyString, doc }) => {
+export default ({ keyString, doc, docId }) => {
   const [toggle, setToggle] = useState(false)
   const [value, setValue] = useState(doc[keyString])
   return (
@@ -24,7 +25,7 @@ export default ({ keyString, doc }) => {
       {toggle &&
         <span>
           <input type='text' value={value} onChange={(ev) => setValue(ev.target.value)} />
-          <button onClick={() => storeDoc(value, doc, keyString)}>save</button>
+          <button onClick={() => storeDoc(value, doc, keyString, setToggle)}>save</button>
           <button onClick={() => setToggle(false)}>cancel</button>
         </span>}
     </>
