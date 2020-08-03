d972187bef000f6b9a4290cc993b7be1f7a2017d
Robert Agthe <robert@scriptshit.de>
2020-08-03 11:43:50 +0200

    maxresdefault is not guaranteed to exist

--- src/utils/getYoutubeImage.js
+++ src/utils/getYoutubeImage.js
@@ -2,7 +2,7 @@ import getYoutubeSlug from './getYoutubeSlug'
 
 export default (link, quality) => {
   if (quality === 'max') {
-    return `https://i3.ytimg.com/vi/${getYoutubeSlug(link)}/maxresdefault.jpg`
+    return `https://i3.ytimg.com/vi/${getYoutubeSlug(link)}/hqdefault.jpg`
   } else {
     return `https://i3.ytimg.com/vi/${getYoutubeSlug(link)}/hqdefault.jpg`
   }
