03ea547040604a10e3f96b9f7f4bb21631346d2c
Robert Agthe <robert@scriptshit.de>
2022-05-24 13:12:11 +0200

    better scaling of youtube videos

--- src/components/challenge-entries/index.js
+++ src/components/challenge-entries/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { map } from 'lodash'
 import Youtube from '../youtube'
 import Typography from '../typography'
+import styles from './styles.module.css'
 
 /**
  * query all rss feeds, and render the individual posts
@@ -9,7 +10,7 @@ import Typography from '../typography'
 export default ({ children, limit, entries }) => {
   return map(entries, (item, i) => {
     return (
-      <div key={'challengeEntry-' + i}>
+      <div className={styles.main} key={'challengeEntry-' + i}>
         <Youtube title={'Submission from ' + item.artist} link={item.yt} />
         <Typography>
           <p><span role='img' aria-label='disk symbol'>💾</span>&nbsp;<a href={item.preset}>Download</a></p>
