59e476dfab81090242e1480d94259ed90b5a7247
Robert Agthe <robert@scriptshit.de>
2022-05-31 14:14:34 +0200

    creating a profile for every new login

--- src/pages/validatemail.js
+++ src/pages/validatemail.js
@@ -3,6 +3,8 @@ import Typography from '../components/typography'
 import Header from '../components/menu-main'
 import SignIn from '../components/sign-in'
 import { Link } from 'gatsby'
+import firebase from 'firebase/app'
+
 const validate = () => {
   // Confirm the link is a sign-in with email link.
   if (window.firebase.auth().isSignInWithEmailLink(window.location.href)) {
@@ -19,9 +21,33 @@ const validate = () => {
     }
     // The client SDK will parse the code from the link for you.
     window.firebase.auth().signInWithEmailLink(email, window.location.href)
-      .then((result) => {
+      .then(async (result) => {
         // Clear email from storage.
         window.localStorage.removeItem('emailForSignIn')
+
+        // current logged in user id
+        const uid = firebase.auth().currentUser.uid
+
+        // prepare the new user profile object
+        const userDoc = {
+          uid: uid,
+          discordUid: '',
+          username: (email).split('@')[0]
+        }
+
+        const db = firebase.firestore()
+
+        // add new user profile doc to firebase
+        try {
+          const docRef = await db
+            .collection('profile')
+            .doc(firebase.auth().currentUser.uid)
+            .set(userDoc)
+          console.log('Document written with ID: ', docRef)
+        } catch (error) {
+          console.error('Error adding document: ', error)
+        }
+
         // You can access the new user via result.user
         // Additional user info profile not available via:
         // result.additionalUserInfo.profile == null
@@ -32,6 +58,7 @@ const validate = () => {
       .catch((error) => {
         // Some error occurred, you can inspect the code: error.code
         // Common errors could be invalid email and invalid or expired OTPs.
+        console.log(error)
       })
   }
 }
@@ -40,14 +67,16 @@ const ValidateMail = () => {
     validate()
   }
 
-  return <div>
-    <Header />
-    <SignIn />
+  return (
+    <div>
+      <Header />
+      <SignIn />
 
-    <Typography>
-      <p>You are logged in now. <Link to='/'>back to the site</Link></p>
-    </Typography>
-         </div>
+      <Typography>
+        <p>You are logged in now. <Link to='/'>back to the site</Link></p>
+      </Typography>
+    </div>
+  )
 }
 
 export default ValidateMail
